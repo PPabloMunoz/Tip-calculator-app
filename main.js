aab4745e4cc9d99335936b4750bc07252652a738
Pablo Muñoz <pablomunoz.0507@gmail.com>
2022-05-12 07:14:04 +0200

    finish proyect

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ tipInput.addEventListener ('click', function () {
     tipBtnInput.checked = true;
 });
 
-var tip = 0;
+var bill = 0;
+var tip = 5;
+var people = 1;
 
 const btn5 = document.getElementById ('btn-check-5');
 const btn10 = document.getElementById ('btn-check-10');
@@ -18,29 +20,75 @@ const inputTip = document.getElementById ('input-tip');
 /* Making the tip variable equal to the value of the button that is clicked. */
 btn5.addEventListener ('click', function () {
     tip = 5;
-    console.log (tip);
+    updateData();
 });
 btn10.addEventListener ('click', function () {
     tip = 10;
-    console.log (tip);
+    updateData();
 });
 btn15.addEventListener ('click', function () {
     tip = 15;
-    console.log (tip);
+    updateData();
 });
 btn25.addEventListener ('click', function () {
     tip = 25;
-    console.log (tip);
+    updateData();
 });
 btn50.addEventListener ('click', function () {
     tip = 50;
-    console.log (tip);
+    updateData();
 });
 inputTip.addEventListener ('input', function () {
-    tip = parseInt(inputTip.value);
-    console.log (tip);
+    tip = parseFloat(inputTip.value);
+    updateData();
 });
 
 
-const bill = document.getElementById ('input-bill').value;
-const people = document.getElementById ('input-people').value;
+const billInput = document.getElementById ('input-bill');
+const peopleInput = document.getElementById ('input-people');
+
+
+billInput.addEventListener ('input', function () {
+    bill = parseFloat(billInput.value);
+    
+    updateData();
+});
+
+peopleInput.addEventListener ('input', function () {
+    people = parseFloat(peopleInput.value);
+    
+    updateData();
+});
+
+var priceTipAmount;
+var priceTotalAmount;
+
+const tipAmount = document.getElementById ("amount-price-tip");
+const totalAmount = document.getElementById ("amount-price-total");
+
+
+
+function updateData() {
+    priceTipAmount = (bill * (tip / 100)) / people;
+    priceTotalAmount = (bill / people) + priceTipAmount;
+    parseFloat(priceTipAmount);
+    parseFloat(priceTotalAmount);
+    if (peopleInput.value == 0) {
+        tipAmount.innerHTML = (`Error`);
+        totalAmount.innerHTML = (`Error`);
+    } else {
+        tipAmount.innerHTML = (`$${priceTipAmount}`);
+        totalAmount.innerHTML = (`$${priceTotalAmount}`);
+    }
+}
+
+
+const btnReset = document.getElementById ('btn-reset');
+
+btnReset.addEventListener ('click', function () {
+    billInput.value = 0;
+    bill = 0;
+    peopleInput.value = 1;
+    people = 1;
+    updateData()
+});
